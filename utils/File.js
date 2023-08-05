dff10661f0440fd756a3af363cf18ffa9486367b
Santiago Cano <sanblackx1@gmail.com>
2023-08-05 01:32:42 -0400

    REFACTOR: File object with error handling on SaveResults and Docs.

--- utils/File.js
+++ utils/File.js
@@ -1,33 +1,44 @@
-const fs = require("fs");
-const path = require("path");
+const fs = require("node:fs");
 
-const Folder = {
-    root: path.dirname(__dirname),
-    testing: `${path.dirname(__dirname)}/data/testing`,
-};
+const Routes = require("./Routes.js");
 
 const File = {
     /**
-     * CreateFolder checks whether a folder exists to create it
-     * @param {string} folder folder name
-     * @param {boolean} is_testing if **true** it would created a path for testing files in the ***./data/testing*** path
-     * @returns path for the created folder
+     * Creates a folder at the specified path. If the folder does not exist, it will be created.
+     *
+     * @async
+     * @param {object} options - The options for creating the folder.
+     * @param {string} [options.folder=""] - The name of the folder to be created (optional).
+     * @param {boolean} [options.is_source=false] - Indicates whether the folder is for testing purposes (optional).
+     * @returns {Promise<string>} - A Promise that resolves to the path of the created folder.
+     * @throws {Error} If there's an issue with creating the folder or any other errors during the process.
+     *
+     * @example
+     * // Create a folder named "myFolder" in the default root directory
+     * const resultPath = await CreateFolder({ folder: "myFolder" });
+     * console.log(`Folder created at: ${resultPath}`);
+     *
+     * // Create inside the **source folder** a folder named "testFolder"
+     * const testPath = await CreateFolder({ folder: "testFolder", is_source: true });
+     * console.log(`Testing folder created at: ${testPath}`);
      */
-    async CreateFolder(folder = "", is_testing = false) {
-        let wanted_path = `${Folder.root}/data/${folder}`;
-        if (is_testing) wanted_path = `${Folder.testing}/${folder}`;
+    async CreateFolder({ folder = "", is_source = false }) {
+        let wanted_path = `${Routes.root}/${folder}`;
+        if (is_source) wanted_path = `${Routes.source}/${folder}`;
 
-        if (fs.existsSync(wanted_path)) {
-            console.log("\x1b[33mFile or directory already created\x1b[0m");
-        } else {
+        if (!fs.existsSync(wanted_path)) {
             await fs.promises.mkdir(wanted_path, { recursive: true });
         }
         return wanted_path;
     },
 
     async SaveResults(notes, path_file = null) {
+        if (!notes || notes.length < 1) {
+            console.error(`Notes must be extracted - Argument Notes: ${notes?.length}`);
+            return;
+        }
         path_file = path_file ?? "./data/generic_results.json";
         await fs.promises.writeFile(path_file, JSON.stringify(notes, null, 4));
     },
 };
-module.exports = { File };
+module.exports = File;
