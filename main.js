461c8b1c1dfbe7653bcae84c5b7668d45fd0179d
Santiago Cano <sanblackx1@gmail.com>
2023-11-06 05:10:50 -0500

    IMPROVE: Cleaning up the main function

--- main.js
+++ main.js
@@ -4,22 +4,17 @@ const path = require("node:path");
 const File = require("./utils/File");
 const Core = require("./utils/Core");
 
-const file_path = path.join(__dirname, "./data/My Clippings.txt");
-const raw_data = fs.readFileSync(file_path, "utf8");
+const clippingFile = path.join(__dirname, "./data/My Clippings.txt");
 
 (async () => {
     // TODO: Sync file -> It must be a function to search into the core.json the last entry
     // as a reference point to Sync the new entries.
 
-    // Default naming files -------------->
-    const converted_file = "core.json";
-    const books = "books.json";
-
-    const root = await File.CreateFolder({ is_source: true });
-
     // Structuring Notes from the Clipping.txt --------------->
-    const notes = Core.DataStructure(raw_data);
-    const separateBooks = Core.GroupBy(notes);
-    await File.SaveResults(notes, `${root}/${converted_file}`);
-    await File.SaveResults(separateBooks, `${root}/${books}`);
+    const timeStart = performance.now();
+    const books = await Core.DataStructure(clippingFile);
+    await Core.formatNoteToMD(books);
+    const timeEnd = performance.now();
+    const secondsElapse = (timeEnd - timeStart) / 1000;
+    console.log(`\x1b[32m All books extracted\x1b[0m => ${secondsElapse.toFixed(4)} seconds.`);
 })();
