1761e098370b146cc23e8be236976407651c98de
Santiago Cano <sanblackx1@gmail.com>
2023-11-07 01:20:18 -0500

    NEW: Sync functionality implemented

--- utils/Sync.mjs
+++ utils/Sync.mjs
@@ -1,35 +1,70 @@
-import fs from "node:fs/promises";
-import path from "node:path";
+import fs from 'node:fs/promises';
 
-import Routes from "./Routes.js";
-import Core from "./Core.js";
+import Routes from './Routes.js';
 
-const SyncEntry = async (processPath = `${Routes.source}/core.json`) => {
-    const raw_data = await fs.readFile(path.join("./data/My Clippings.txt"), "utf8");
+const SyncNote = {
+  HasSyncFile: async function (processPath = Routes.tempFile) {
+    let existsFile;
     try {
-        let foundIndex;
-        await fs.access(processPath);
-        const prevCore = JSON.parse(await fs.readFile(processPath, "utf8"));
-        const currentCore = Core.DataStructure(raw_data);
-
-        const lastPrevEntry = prevCore[prevCore.length - 1];
-        const indexCurrentEntry = currentCore.length - 1;
-
-        if (lastPrevEntry.hasOwnProperty("highlight")) {
-            foundIndex = currentCore.findIndex((n) => n.highlight === lastPrevEntry.highlight);
-        } else {
-            foundIndex = currentCore.findIndex((n) => n.note === lastPrevEntry.note);
-        }
-
-        if (!foundIndex || foundIndex === indexCurrentEntry) {
-            console.info("Already Sync");
-            return;
-        }
+      await fs.access(processPath);
+      existsFile = true;
     } catch (error) {
-        console.error("\x1b[33mProcessed file cannot be access\x1b[0m");
-        console.log(error);
-        process.exit(1);
+      existsFile = false;
     }
+
+    return { existsFile, processPath };
+  },
+
+  /**
+   *
+   * @param {ExtractedNotes} currentNotes
+   * @param {*} clippingFile
+   * @returns
+   */
+  SyncEntry: async function (currentNotes) {
+    const { existsFile, processPath } = await this.HasSyncFile();
+    if (!existsFile) return false;
+
+    try {
+      let foundIndex;
+
+      /**
+       * @type {NoteResults}
+       */
+      const prevCore = JSON.parse(await fs.readFile(processPath, 'utf8'));
+
+      const lastPrevEntry = prevCore.lastEntry;
+      const lastCurrentEntry = currentNotes.length - 1;
+
+      // Finding match by Highlight or Note
+      if (lastPrevEntry['highlight']) {
+        foundIndex = currentNotes.findIndex((n) => {
+          const matchHigh = n.highlight === lastPrevEntry.highlight;
+          const matchRef = n.reference === lastPrevEntry.reference;
+          return matchHigh && matchRef;
+        });
+      } else {
+        foundIndex = currentNotes.findIndex((n) => {
+          const matchNote = n.note === lastPrevEntry.note;
+          const matchRef = n.reference == lastPrevEntry.reference;
+
+          return matchNote && matchRef;
+        });
+      }
+
+      console.log({ lastPrevEntry, lastCurrentEntry, foundIndex });
+      if (foundIndex === lastCurrentEntry) {
+        console.info('Already Sync');
+        return true;
+      } else {
+        return false;
+      }
+    } catch (error) {
+      console.error('\x1b[33mProcessed file cannot be access\x1b[0m');
+      console.log(error);
+      // process.exit(1);
+    }
+  }
 };
 
-await SyncEntry();
+export default SyncNote;
